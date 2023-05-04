134cdf9a0e03ff167e28c16b950f0de90301811e
dokkoh <ceedje74@gmail.com>
2023-05-04 17:46:49 +0200

    avance css et menu burger a terminer

--- src/js/global.js
+++ src/js/global.js
@@ -1,3 +1,3 @@
 import feather from 'feather-icons';
-
+import * as menu from './menu.js'
 feather.replace();
---
+++ src/js/menu.js
@@ -0,0 +1,4 @@
+const toggler = document.getElementById("toggler");
+toggler.addEventListener("click", () => {
+  document.body.classList.toggle("menu--on");
+});
